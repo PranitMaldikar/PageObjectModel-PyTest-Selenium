ccdaa2f7b0a30b55ad300df800e7fb7ababb8694
Pranit Maldikar <pranitmaldikar@gmail.com>
2024-06-07 22:21:29 -0400

    added sample tests

---
+++ Config/config.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.chrome.service import Service
+class TestData:
+    # s = 
+    CHROME_EXECUTABLE_PATH = Service("C:\\Users\\prani\\Desktop\\Job grind\\pytest\\Portfolio-Test-Automator\\chromedriver.exe")
+
+    BASE_URL = "https://app.hubspot.com/login"
+    USER_NAME = "vincylicdrake2@gmail.com"
+    PASSWORD = "Vincy@123456789"
+
+    LOGIN_PAGE_TITLE = "HubSpot Login and Sign in"
